e61b5b43ddc82518dcf75380b8c7e5c8d1337243
Emanuel Velazquez <evelaz4.93@gmail.com>
2024-06-20 11:55:06 -0400

    Adjusted env file

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -11,6 +13,15 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT,
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true, // Enable SSL connection
+            rejectUnauthorized: false, // For self-signed certificates
+          },
+        }
+      : {},
+    logging: (msg) => console.log(msg), // Enable logging for debugging
   }
 );
 
